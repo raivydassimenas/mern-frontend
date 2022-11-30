85bb4f5ebae0ea2eafd665690edfc3076b713744
Raivydas Šimėnas <raivydas.simenas@gmail.com>
2022-11-30 10:39:58 +0200

    Implement some JWT functionality

---
+++ src/pages/Login.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { FaSignInAlt } from 'react-icons/fa';
+
+function Login() {
+
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const { email, password } = formData;
+
+    const onChange = (e) => {
+        setFormData((prevState) => (
+            {
+                ...prevState,
+                [e.target.name]: e.target.value
+            }
+        )
+        );
+    };
+
+    return (
+        <>
+            <section className='heading'>
+                <h1>
+                    <FaSignInAlt />Login
+                    <p>Please login</p>
+                </h1>
+            </section>
+            <section>
+                <form>
+                    <div className='form-group'>
+                        <input type="email" className='form-control' id='email' name='email' value={email} placeholder="Enter your email" onChange={onChange} />
+                    </div>
+                    <div className='form-group'>
+                        <input type="password" className='form-control' id='password' name='password' value={password} placeholder="Enter your password" onChange={onChange} />
+                    </div>
+                    <div className='form-group'>
+                        <button type='submit' className='btn btn-block'>
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </section>
+        </>
+    );
+};
+
+export default Login;
